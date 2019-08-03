478f2738671b270450cb6a13e4d234a6d9d88c89
Jaine Senna <39140368+jainesenna@users.noreply.github.com>
2019-08-03 12:22:01 -0300

    Add files via upload

---
+++ imprimir.rb
@@ -0,0 +1,8 @@
+=begin
+  Podemos imprimir uma mensagem com ruby 
+  utilizando pelo menos 3 opções: puts, print e p.
+=end
+
+puts "Olá Mundo" #imprime o conteúdo e pula uma linha
+print "Olá Mundo" #imprime, mas não pula linha
+p "Olá Mundo" #chama o método inspect do elemento
